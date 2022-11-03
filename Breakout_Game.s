8678084a0639b00dd44e4f82df9f7c39310fac4f
Szymon Borkowski <szymonborkowski10@gmail.com>
2022-11-03 16:02:29 +0000

    initial setup stage 1

--- Breakout_Game.s
+++ Breakout_Game.s
@@ -1,30 +1,53 @@
+# Breakout game
+# Szymon Borkowski, Kaden Suderman
+# Oct 2022
+
+# ====== Register allocation START ======
+# x0 always = 0
+# x1 return address
+# x2 stack pointer (when used)
+# x3 IOIn(31:0) switches, address 0x00030008
+# x4 program variable
+# x5 memory address
+# x6 dlyCount
+# x7 counter variable
+# x8 paddleNumDlyCount
+# x9 ballNumDlyCount
+# x10 X/YAdd ref - The Y Location of the ball       
+# x11 Not used
+# x12 Not used 
+# x13 Not used
+# x14 zone
+# x15 dlyCountMax. 12.5MHz clock frequency. Two instructions per delay cycle => 6,250,000 delay cycles per second, 625,000 (0x98968) delay cycles per 100msec
+
+# Wall
+#  x16 wallVec value, default 0xffffffff - Stores the condition of the wall
+# Ball
+#  x17 ballXLocationVec
+#  x18 CSBallXAdd (4:0)
+#  x19 NSBallXAdd (4:0)
+#  x20 CSBallYAdd (4:0)
+#  x21 NSBallYAdd (4:0)
+#  x22 CSBallDir  (2:0)
+#  x23 NSBallDir  (2:0)
+#  x24 ballNumDlyMax (4:0)
+# Paddle
+#  x25 paddleXLocationVec
+#  x26 paddleSize (5)
+#  x27 paddleXAddLSB
+#  x28 paddleNumDlyMax (4:0)
+# Score and Lives 
+#  x29 Score
+#  x30 Lives
+# x31  Not used
+# ====== Register allocation END ======
 
 # v================ GAME SETUP ================v
 setup:
+  # jal x1, clearArena
   jal x1, initPaddle
   jal x1, initBall
   jal x1, initWall
-  jal x1, clearArena
-
-initPaddle: # init 5-bit paddle in row 2 
-  addi x26, x0,  0x1f  # load paddle length
-  add  x25, x0,  x26   # add paddle into paddle vec
-  slli x25, x25, 14    # move paddle to middle
-  addi x5, x0,  0x8    # 
-  sw   x10, 0(x11)
-  jalr x0,  0(x1) 
-
-initBall: # 1-bit ball row 3 
-  addi x10, x0,  0x1
-  slli x10, x10, 16
-  addi x12, x0,  0xC
-  sw   x10, 0(x12)
-  jalr x0,  0(x1)
-  
-initWall: # 32-bit wall row 15
-  addi x10, x0, -1
-  addi x13, x0, 0x3C
-  sw   x10, 0(x13)
 
 clearArena: # clear entire memory
   addi x5, x0, 0      # base memory address
@@ -36,6 +59,26 @@ clearArena: # clear entire memory
 	addi x4, x4, 1      # increment loop counter 	
 	ble  x4, x7, clearMemLoop  
   jalr x0, 0(x1)      # ret
+
+initPaddle: # init 5-bit paddle in row 2 
+  addi x26, x0,  0x1f  # create paddle length
+  add  x25, x0,  x26   # add paddle into paddle vec
+  slli x25, x25, 14    # move paddle to middle
+  addi x5, x0,  0x8    # row 2 address
+  sw   x25, 0(x5)      # load paddle into game
+  jalr x0,  0(x1) 
+
+initBall: # 1-bit ball row 3 
+  addi x17, x0,  0x1   # create ball
+  slli x17, x17, 16    # shift ball into middle
+  addi x5, x0,  0xC   
+  sw   x17, 0(x5)
+  jalr x0,  0(x1)
+  
+initWall: # 32-bit wall row 15
+  addi x16, x0, -1
+  addi x5, x0, 0x3C
+  sw   x16, 0(x5)
 
 # ^============================================^
 
